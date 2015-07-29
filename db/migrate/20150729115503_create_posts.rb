09528bcdcec8a6b8c83ffc84df6ff47f1f7ab285
Adrian Shepelavey <adrianshepelavey@gmail.com>
2015-07-29 17:41:00 -0400

    tweaks

--- db/migrate/20150729115503_create_posts.rb
+++ db/migrate/20150729115503_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration
       t.string :title
       t.string :author
       t.string :text
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/20150729115859_create_comments.rb
+++ db/migrate/20150729115859_create_comments.rb
@@ -2,7 +2,9 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string :title
+      t.string :author
       t.string :text
+      t.timestamps null: false
       t.references :post, index: true, foreign_key: true
     end
   end
